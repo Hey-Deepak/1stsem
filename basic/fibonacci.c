e7e8d19395001488dee0c2b81d26b44462b74993
nisha <56124043+nisha0324@users.noreply.github.com>
2019-11-06 22:22:56 +0530

    Update fibonacci.c

--- basic/fibonacci.c
+++ basic/fibonacci.c
@@ -1,10 +1,12 @@
-//Title: to find the factorial of a given number
-/* description: we have to find the factorial of a given number
-                which is done multiplying the given number with
-                the numer receding by one
-
-            eg: number = 5
-                factorial of 5 is = 5*4*3*2*1=120
+//Title: to print the fibonacci series
+/* description: this is what fibonacci series looks like:
+                   eg:0
+                      0   (that is the addition of the two consecutive precedence value )
+                      1    
+                      1
+                      2
+                      3
+                      5 
 */
 
 //------------------------------------------------------------------------------
@@ -13,27 +15,26 @@
 
 int main()
 {
-    int num, i , a=0 , b=1 , sum;
+    int num, i, a = 0, b = 1, sum;
 
     printf("enter the term of series you want to print:");
     scanf("%d", &num);
 
-    printf("%d\n",a);
-    printf("%d\n",b);
+    printf("%d\n", a);
+    printf("%d\n", b);
 
-    for ( i = 3; i <= num; i++)
+    for (i = 3; i <= num; i++)
     {
-       
-        sum=a+b;
-        printf("%d\n",sum);
-        a=b;
-        b=sum;
-        
+
+        sum = a + b;
+        printf("%d\n", sum);
+        a = b;
+        b = sum;
     }
-     
+
     return 0;
 }
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
- //by me:) (nisha)
+//by me:) (nisha)
